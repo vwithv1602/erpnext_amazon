6dc759684d7c69156c8f8f9ae2bcfe6e557a7a1c
khopadi <it@usedyetnew.com>
2019-05-04 20:48:04 +0530

    Incorrect Amazon FBA counts #21

--- erpnext_amazon/erpnext_amazon/report/item_not_listed_new/item_not_listed_new.py
+++ erpnext_amazon/erpnext_amazon/report/item_not_listed_new/item_not_listed_new.py
@@ -132,7 +132,7 @@ class ItemAmazonReport(object):
 				for line in res_array[1:]:
 					# if i > 0 and i < len(res_array)-1:
 					res_line = re.split(r'\t+', line)
-					if res_line[3] == 'Unknown'or res_line[5] == 'Unknown':
+					if res_line[3] == 'Unknown'or res_line[4] == 'Unknown':
 						continue
 					result[res_line[2]] = int(res_line[10]) + int(res_line[12])
 					amazon_prod_ids.append(res_line[1])
