f75962dc781174375f0e00c1d075af3176d94b2e
khopadi <it@usedyetnew.com>
2019-06-04 19:38:24 +0530

    discrepancy in Amazon report. #23

--- erpnext_amazon/erpnext_amazon/report/item_not_listed_new/item_not_listed_new.py
+++ erpnext_amazon/erpnext_amazon/report/item_not_listed_new/item_not_listed_new.py
@@ -132,13 +132,14 @@ class ItemAmazonReport(object):
 				reportResult = get_request('get_report',{'ReportId':generated_report_id})
 				res_array = re.split(r'\n+', reportResult)
 				i = 0
+				#vwrite(res_array)
 				for line in res_array[1:]:
 					# if i > 0 and i < len(res_array)-1:
 					res_line = re.split(r'\t+', line)
 					#vwrite(res_line)
 					if res_line[3] == 'Unknown' or res_line[4] == 'Unknown':
 						continue
-					result[res_line[2]] = int(res_line[10]) + int(res_line[12])
+					result[res_line[2]] = int(res_line[9]) + int(res_line[11])
 					amazon_prod_ids.append(res_line[1])
 				i = i+1
 				break
